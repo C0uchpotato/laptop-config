d7140a9db1e7c8838bb7cf9044c32506210f9bb0
Jack Helmer <rjacksonhelmer@gmail.com>
2023-03-03 12:44:04 -0500

    refresh
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh
    
    refresh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#6-18 is dotbot boilerplate
+#DO NOT EDIT (without proper knowledge)
+
+set -e
+
+CONFIG="install.conf.yaml"
+DOTBOT_DIR="dotbot"
+
+DOTBOT_BIN="bin/dotbot"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "${BASEDIR}"
+git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
+git submodule update --init --recursive "${DOTBOT_DIR}"
+
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+
+
+# Checks if pamanfile is installed from AUR
+# Useful for reinstalls
+
+if pacman -Qm | grep "pacmanfile" ; then
+	echo "pacmanfile installed"
+else
+	yay pacmanfile		
+fi
+
+#installs all programs listed in the "~/.config/pacmanfile/pacmanfile-dumped.txt
+pacmanfile sync
--- update.sh
+++ update.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-#
+
+DIR=~/.config/pacmanfile
+
+#Make pacmanfile dump in github folder
 pacmanfile dump >> pacmanfile-dumped.txt
+
+#Make pacmanfile dump on local computer
+if [ -d "$DIR" ] ; then
+	echo "~/.config/pacmanfile/ exists"
+else
+	mkdir ~/.config/pacmanfile
+fi
+
+#move dump to local computer
+cp pacmanfile-dumped.txt ~/.config/pacmanfile/
+
+#git auto commit & push
 git add *
 git commit -m "refresh"
 git push
